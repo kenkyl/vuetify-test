ab9c4b2ade6eedf606380512bcf6a58f570cb4a5
Kyle Kennedy <kylekennedy@Kyles-MBP.attlocal.net>
2020-04-06 12:12:42 -0500

    got basic dark theme layout

---
+++ src/components/ProducerComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card>
+        
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "ProducerComponent",
+    props: [
+        "message"
+    ],
+    data() {
+        return {
+            id: ''
+        }
+    },
+    created: function() {
+        console.log("Producer is in the building, wuba luba dub dub!");
+        this.id = Math.random() * 1000;
+    }
+}
+</script>
